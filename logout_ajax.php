1ebb8b828fa7d937fb4b8b1b8a7071538782b63c
Allen Nikka <allennikka@wustl.edu>
2016-10-29 16:12:24 -0500

    Logout now works without refreshing the page.

--- logout_ajax.php
+++ logout_ajax.php
@@ -1,11 +1,21 @@
 <?php
-session_unset();
-session_destroy();
-header("Content-Type: application/json"); // Since we are sending a JSON response here (not an HTML document), set the MIME Type to application/json
-
-echo json_encode(array(
-        "success" => true
-));
-exit;
-
+// session_unset();
+// session_destroy();
+// header("Content-Type: application/json"); // Since we are sending a JSON response here (not an HTML document), set the MIME Type to application/json
+//
+// echo json_encode(array(
+//         "success" => true
+// ));
+// exit;
+if ($_GET["argument"]=='logOut'){
+    if(session_id() == '') {
+        session_start();
+    }
+    session_unset();
+    session_destroy();
+    // $host  = $_SERVER['HTTP_HOST'];
+    // $link = "http://$host/calendar_base.html";
+    // echo $link;
+    exit;
+}
 ?>
